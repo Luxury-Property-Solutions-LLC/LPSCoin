b3203236533ac43d3ad2c1b374dcf6593455d2fe
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:57:49 -0500

    Update xtrans.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/xtrans.mk
+++ depends/packages/xtrans.mk
@@ -1,22 +1,27 @@
 package=xtrans
-$(package)_version=1.3.4
-$(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=054d4ee3efd52508c753e9f7bc655ef185a29bd2850dd9e2fc2ccc33544f583a
+$(package)_version=1.5.0
+$(package)_download_path=https://xorg.freedesktop.org/releases/individual/lib/
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash=1ba4b1a5f2d2f2f58ca3b2d5d599cd73bd00f370b88b6cd998fbd6bc15e576f0
 $(package)_dependencies=
 
 define $(package)_set_vars
-$(package)_config_opts_linux=--with-pic --disable-static
+  $(package)_config_opts=--disable-shared --prefix=$(host_prefix)
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install || exit 1
+endef
+
+define $(package)_postprocess_cmds
+  rm -f $($(package)_staging_dir)$(host_prefix)/lib/*.la || exit 1
 endef
