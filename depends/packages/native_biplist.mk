5ceb46dc00cbdf976844af9bb9d338ac79982e0b
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:18:05 -0500

    Update native_biplist.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/native_biplist.mk
+++ depends/packages/native_biplist.mk
@@ -1,15 +1,15 @@
 package=native_biplist
-$(package)_version=0.9
-$(package)_download_path=https://files.pythonhosted.org/packages/8e/60/dd2e9f8c310289b3055351f0938d8342da2ea47e2894ac881c70f87a491b/
+$(package)_version=1.0.3
+$(package)_download_path=https://files.pythonhosted.org/packages/source/b/biplist/
 $(package)_file_name=biplist-$($(package)_version).tar.gz
-$(package)_sha256_hash=b57cadfd26e4754efdf89e9e37de87885f9b5c847b2615688ca04adfaf6ca604
-$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
+$(package)_sha256_hash=ec7e4f7df0d0c71b00ed9a7574866518e93d31c1b3170e8a7c9cf442c369b0f6
+$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
-    python setup.py build
+  python3 setup.py build || exit 1
 endef
 
 define $(package)_stage_cmds
-    mkdir -p $($(package)_install_libdir) && \
-    python setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
+  mkdir -p $($(package)_install_libdir) && \
+  python3 setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir) || exit 1
 endef
