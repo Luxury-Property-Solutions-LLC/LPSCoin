33d8785c8ae74dbaa56596416aa372eecbfbe923
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 22:24:03 -0500

    Update fontconfig.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/fontconfig.mk
+++ depends/packages/fontconfig.mk
@@ -1,12 +1,16 @@
 package=fontconfig
-$(package)_version=2.11.1
-$(package)_download_path=http://www.freedesktop.org/software/fontconfig/release/
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=dc62447533bca844463a3c3fd4083b57c90f18a70506e7a9f4936b5a1e516a99
-$(package)_dependencies=freetype expat
+$(package)_version:=2.15.0
+$(package)_download_path:=https://www.freedesktop.org/software/fontconfig/release/
+$(package)_file_name:=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash:=6be0ee38f9c24fe225a932e34871992773eeea8e64f2d6d103d37c09de52eb2b
+$(package)_dependencies:=freetype expat
+$(package)_autoconf:=configure
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-docs --disable-static
+  $(package)_config_opts:=--disable-docs --disable-shared
+  $(package)_config_opts+=--with-freetype=$($(package)_staging_prefix_dir)
+  $(package)_config_opts+=--with-expat=$($(package)_staging_prefix_dir)
+  $(package)_config_opts_linux:=--with-pic
 endef
 
 define $(package)_config_cmds
@@ -14,9 +18,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install || exit 1
 endef
