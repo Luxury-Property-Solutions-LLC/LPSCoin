00612a6d523cd3920360698402e8b2322bd4b1fc
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 22:14:49 -0500

    Update boost.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,35 +1,35 @@
 package=boost
-$(package)_version=1_59_0
-$(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.59.0
-$(package)_file_name=$(package)_$($(package)_version).tar.bz2
-$(package)_sha256_hash=727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca
+$(package)_version:=1_84_0
+$(package)_download_path:=https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source
+$(package)_file_name:=$(package)_$($(package)_version).tar.bz2
+$(package)_sha256_hash:=cc4b893acf2557060ea240c37e66ea1d939ed0a6ae1f08b63d831dc3287589e6
 
 define $(package)_set_vars
-$(package)_config_opts_release=variant=release
-$(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
-$(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
-$(package)_config_opts_linux=threadapi=pthread runtime-link=shared
-$(package)_config_opts_darwin=--toolset=darwin-4.2.1 runtime-link=shared
-$(package)_config_opts_mingw32=binary-format=pe target-os=windows threadapi=win32 runtime-link=static
-$(package)_config_opts_x86_64_mingw32=address-model=64
-$(package)_config_opts_i686_mingw32=address-model=32
-$(package)_config_opts_i686_linux=address-model=32 architecture=x86
-$(package)_toolset_$(host_os)=gcc
-$(package)_archiver_$(host_os)=$($(package)_ar)
-$(package)_toolset_darwin=darwin
-$(package)_archiver_darwin=$($(package)_libtool)
-$(package)_config_libraries=chrono,filesystem,program_options,system,thread,test
-$(package)_cxxflags=-std=c++11 -fvisibility=hidden
-$(package)_cxxflags_linux=-fPIC
+  $(package)_config_opts_release:=variant=release
+  $(package)_config_opts_debug:=variant=debug
+  $(package)_config_opts:=--layout=tagged --build-type=complete --user-config=user-config.jam
+  $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1 runtime-link=static
+  $(package)_config_opts_linux:=threadapi=pthread
+  $(package)_config_opts_darwin:=--toolset=clang
+  $(package)_config_opts_mingw32:=binary-format=pe target-os=windows threadapi=win32
+  $(package)_config_opts_x86_64_mingw32:=address-model=64
+  $(package)_config_opts_i686_mingw32:=address-model=32
+  $(package)_config_opts_i686_linux:=address-model=32 architecture=x86
+  $(package)_toolset_$(host_os):=gcc
+  $(package)_archiver_$(host_os):=$($(package)_ar)
+  $(package)_toolset_darwin:=clang
+  $(package)_archiver_darwin:=$($(package)_libtool)
+  $(package)_config_libraries:=chrono,filesystem,program_options,system,thread,test
+  $(package)_cxxflags:=-std=c++11 -fvisibility=hidden
+  $(package)_cxxflags_linux:=-fPIC
 endef
 
 define $(package)_preprocess_cmds
-  echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
+  echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\" <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam || exit 1
 endef
 
 define $(package)_config_cmds
-  ./bootstrap.sh --without-icu --with-libraries=$(boost_config_libraries)
+  ./bootstrap.sh --without-icu --with-libraries=$($(package)_config_libraries)
 endef
 
 define $(package)_build_cmds
