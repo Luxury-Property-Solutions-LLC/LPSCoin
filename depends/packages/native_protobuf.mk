bf9e561f195929185161914d5723be6119f4cdc1
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:30:18 -0500

    Update native_protobuf.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,25 +1,25 @@
 package=native_protobuf
-$(package)_version=2.6.1
-$(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
-$(package)_file_name=protobuf-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ee445612d544d885ae240ffbcbf9267faa9f593b7b101f21d58beceb92661910
+$(package)_version=3.25.3
+$(package)_download_path=https://github.com/protocolbuffers/protobuf/releases/download/v$($(package)_version)
+$(package)_file_name=protobuf-$($(package)_version).tar.gz
+$(package)_sha256_hash=2e2f0f6b9f3f8f294ba29e44e4c316675ddbff8e5507500c14109c1d8260e4e0
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared
+  $(package)_config_opts=--disable-shared --prefix=$(build_prefix)
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src protoc
+  $(MAKE) -C src protoc || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C src DESTDIR=$($(package)_staging_dir) install-strip
+  $(MAKE) -C src DESTDIR=$($(package)_staging_dir) install-strip || exit 1
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf lib include
+  rm -rf $($(package)_staging_dir)$(build_prefix)/{lib,include} || exit 1
 endef
