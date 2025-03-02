3836f52951a46f01bcce72bcbde47c68a94d757d
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:36:33 -0500

    Update qrencode.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -1,22 +1,22 @@
 package=qrencode
-$(package)_version=3.4.4
+$(package)_version=4.1.1
 $(package)_download_path=https://fukuchi.org/works/qrencode/
-$(package)_file_name=qrencode-$(qrencode_version).tar.bz2
-$(package)_sha256_hash=efe5188b1ddbcbf98763b819b146be6a90481aac30cfc8d858ab78a19cde1fa5
+$(package)_file_name=qrencode-$($(package)_version).tar.gz
+$(package)_sha256_hash=141c3c6c4b64f7e6aa42d9122b5eAshley1c6ae6e9b8ae4e5b4ea5c8e
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared -without-tools --disable-sdltest
-$(package)_config_opts_linux=--with-pic
+  $(package)_config_opts=--disable-shared --without-tools --disable-sdltest --prefix=$(host_prefix)
+  $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install || exit 1
 endef
