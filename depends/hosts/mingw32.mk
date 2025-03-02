18924c52db98776b86e51b7b3093443b68e284e6
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 22:02:19 -0500

    Update mingw32.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/hosts/mingw32.mk
+++ depends/hosts/mingw32.mk
@@ -1,10 +1,7 @@
-mingw32_CFLAGS=-pipe
-mingw32_CXXFLAGS=$(mingw32_CFLAGS)
-
-mingw32_release_CFLAGS=-O2
-mingw32_release_CXXFLAGS=$(mingw32_release_CFLAGS)
-
-mingw32_debug_CFLAGS=-O1
-mingw32_debug_CXXFLAGS=$(mingw32_debug_CFLAGS)
-
-mingw32_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC
+ifeq ($(host_arch),i686)
+mingw32_CC := i686-w64-mingw32-gcc
+mingw32_CXX := i686-w64-mingw32-g++
+else ifeq ($(host_arch),x86_64)
+mingw32_CC := x86_64-w64-mingw32-gcc
+mingw32_CXX := x86_64-w64-mingw32-g++
+endif
