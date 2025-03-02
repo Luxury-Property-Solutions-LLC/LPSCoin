6ca2f4b10ea0a4f445b45bdeace27eb15bc3db0e
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:34:58 -0500

    Update protobuf.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/protobuf.mk
+++ depends/packages/protobuf.mk
@@ -1,29 +1,27 @@
 package=protobuf
-$(package)_version=$(native_$(package)_version)
+$(package)_version=$(native_$(package)_version)  # e.g., 3.25.3
 $(package)_download_path=$(native_$(package)_download_path)
 $(package)_file_name=$(native_$(package)_file_name)
 $(package)_sha256_hash=$(native_$(package)_sha256_hash)
 $(package)_dependencies=native_$(package)
-$(package)_cxxflags=-std=c++11
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --with-protoc=$(build_prefix)/bin/protoc
+  $(package)_config_opts=--disable-shared --with-protoc=$(build_prefix)/bin/protoc --prefix=$(host_prefix)
   $(package)_config_opts_linux=--with-pic
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C src libprotobuf.la
+  $(MAKE) -C src libprotobuf.la || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) -C src install-libLTLIBRARIES install-nobase_includeHEADERS &&\
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install-pkgconfigDATA
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -C src install || exit 1
 endef
 
 define $(package)_postprocess_cmds
-  rm lib/libprotoc.a
+  rm -f $($(package)_staging_dir)$(host_prefix)/lib/libprotoc.a || exit 1
 endef
