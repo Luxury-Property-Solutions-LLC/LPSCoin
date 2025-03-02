1ca244d5171a37d1f0fc9e9a199b1ff6c0a69993
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:24:33 -0500

    Update native_ds_store.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/native_ds_store.mk
+++ depends/packages/native_ds_store.mk
@@ -1,17 +1,17 @@
 package=native_ds_store
-$(package)_version=1.1.0
+$(package)_version=1.3.0
 $(package)_download_path=https://github.com/al45tair/ds_store/archive/refs/tags/
-$(package)_download_file=v1.1.0.tar.gz
-$(package)_file_name=v1.1.0.tar.gz
-$(package)_sha256_hash=a9f4c0755c6be7224ff7029e188dd262e830bb81e801424841db9eb0780ec8ed
-$(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
+$(package)_download_file=v$($(package)_version).tar.gz
+$(package)_file_name=ds_store-$($(package)_version).tar.gz
+$(package)_sha256_hash=628f74a398c59e2d97e8cc2fb076c87e1bf4ff0b8f4ba90f6a18b6c4815f9872
+$(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 $(package)_dependencies=native_biplist
 
 define $(package)_build_cmds
-    python setup.py build
+  python3 setup.py build || exit 1
 endef
 
 define $(package)_stage_cmds
-    mkdir -p $($(package)_install_libdir) && \
-    python setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir)
+  mkdir -p $($(package)_install_libdir) || exit 1 && \
+  python3 setup.py install --root=$($(package)_staging_dir) --prefix=$(build_prefix) --install-lib=$($(package)_install_libdir) || exit 1
 endef
