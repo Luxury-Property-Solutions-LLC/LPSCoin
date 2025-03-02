8e9ac1917212cd7017993f1ed300916ec189efe4
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 22:48:47 -0500

    Update libXau.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/libXau.mk
+++ depends/packages/libXau.mk
@@ -1,13 +1,14 @@
 package=libXau
-$(package)_version=1.0.8
-$(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2
-$(package)_dependencies=xproto
+$(package)_version:=1.0.11
+$(package)_download_path:=http://xorg.freedesktop.org/releases/individual/lib/
+$(package)_file_name:=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash:=f3fa3282f526e3252c8452b0d520ed18532be01f4293774cd81a3aa6f8aa5192
+$(package)_dependencies:=xproto
+$(package)_autoconf:=configure
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
-  $(package)_config_opts_linux=--with-pic
+  $(package)_config_opts:=--disable-shared
+  $(package)_config_opts_linux:=--with-pic
 endef
 
 define $(package)_config_cmds
@@ -15,9 +16,9 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install || exit 1
 endef
