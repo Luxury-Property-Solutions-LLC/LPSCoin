f6e896a429b6636598d008119cb57032ab75d255
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:13:13 -0500

    Update libXext.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/libXext.mk
+++ depends/packages/libXext.mk
@@ -1,22 +1,22 @@
 package=libXext
-$(package)_version=1.3.2
-$(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
+$(package)_version=1.3.6
+$(package)_download_path=https://xorg.freedesktop.org/archive/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=f829075bc646cdc085fa25d98d5885d83b1759ceb355933127c257e8e50432e0
+$(package)_sha256_hash=1d7a9ba3640f843fe1bb15b80fb3902f30eb9f2e6e7fc7b5f1e0f6cd5f87c307
 $(package)_dependencies=xproto xextproto libX11 libXau
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-static
+  $(package)_config_opts=--disable-static --prefix=$($(package)_staging_prefix_dir)
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install || exit 1
 endef
