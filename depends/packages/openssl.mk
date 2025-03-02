d7b819fa8a8d7b21e40a9e09dc764c15d9795f81
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:32:22 -0500

    Update openssl.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,78 +1,49 @@
 package=openssl
-$(package)_version=1.0.1k
+$(package)_version=1.1.1w
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=8f9faeaebad088e772f4ef5e38252d472be4d878c6b3a2718c10a4fcebe7a41c
+$(package)_sha256_hash=cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d9077bfa037b993
 
 define $(package)_set_vars
-$(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
-$(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
-$(package)_config_opts+=no-camellia
-$(package)_config_opts+=no-capieng
-$(package)_config_opts+=no-cast
-$(package)_config_opts+=no-comp
-$(package)_config_opts+=no-dso
-$(package)_config_opts+=no-dtls1
-$(package)_config_opts+=no-ec_nistp_64_gcc_128
-$(package)_config_opts+=no-gost
-$(package)_config_opts+=no-gmp
-$(package)_config_opts+=no-heartbeats
-$(package)_config_opts+=no-idea
-$(package)_config_opts+=no-jpake
-$(package)_config_opts+=no-krb5
-$(package)_config_opts+=no-libunbound
-$(package)_config_opts+=no-md2
-$(package)_config_opts+=no-mdc2
-$(package)_config_opts+=no-rc4
-$(package)_config_opts+=no-rc5
-$(package)_config_opts+=no-rdrand
-$(package)_config_opts+=no-rfc3779
-$(package)_config_opts+=no-rsax
-$(package)_config_opts+=no-sctp
-$(package)_config_opts+=no-seed
-$(package)_config_opts+=no-sha0
-$(package)_config_opts+=no-shared
-$(package)_config_opts+=no-ssl-trace
-$(package)_config_opts+=no-ssl2
-$(package)_config_opts+=no-ssl3
-$(package)_config_opts+=no-static_engine
-$(package)_config_opts+=no-store
-$(package)_config_opts+=no-unit-test
-$(package)_config_opts+=no-weak-ssl-ciphers
-$(package)_config_opts+=no-whirlpool
-$(package)_config_opts+=no-zlib
-$(package)_config_opts+=no-zlib-dynamic
-$(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
-$(package)_config_opts_linux=-fPIC -Wa,--noexecstack
-$(package)_config_opts_x86_64_linux=linux-x86_64
-$(package)_config_opts_i686_linux=linux-generic32
-$(package)_config_opts_arm_linux=linux-generic32
-$(package)_config_opts_aarch64_linux=linux-generic64
-$(package)_config_opts_mipsel_linux=linux-generic32
-$(package)_config_opts_mips_linux=linux-generic32
-$(package)_config_opts_powerpc_linux=linux-generic32
-$(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
-$(package)_config_opts_x86_64_mingw32=mingw64
-$(package)_config_opts_i686_mingw32=mingw
+  $(package)_config_env=AR="$($(package)_ar)" RANLIB="$($(package)_ranlib)" CC="$($(package)_cc)"
+  $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
+  $(package)_config_opts+=no-camellia no-capieng no-cast no-comp no-dso no-dtls1
+  $(package)_config_opts+=no-ec_nistp_64_gcc_128 no-gost no-gmp no-heartbeats no-idea
+  $(package)_config_opts+=no-jpake no-krb5 no-libunbound no-md2 no-mdc2 no-rc4
+  $(package)_config_opts+=no-rc5 no-rdrand no-rfc3779 no-rsax no-sctp no-seed
+  $(package)_config_opts+=no-sha0 no-shared no-ssl-trace no-static_engine no-store
+  $(package)_config_opts+=no-unit-test no-weak-ssl-ciphers no-whirlpool no-zlib
+  $(package)_config_opts+=no-zlib-dynamic $($(package)_cflags) $($(package)_cppflags)
+  $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
+  $(package)_config_opts_x86_64_linux=linux-x86_64
+  $(package)_config_opts_i686_linux=linux-elf
+  $(package)_config_opts_arm_linux=linux-armv4
+  $(package)_config_opts_aarch64_linux=linux-aarch64
+  $(package)_config_opts_mipsel_linux=linux-mips32
+  $(package)_config_opts_mips_linux=linux-mips32
+  $(package)_config_opts_powerpc_linux=linux-ppc
+  $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
+  $(package)_config_opts_x86_64_mingw32=mingw64
+  $(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
-  sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
-  sed -i.old "s|engines apps test|engines|" Makefile.org
+  sed -i.old "/define DATE/d" util/mkbuildinf.pl || exit 1 && \
+  sed -i.old "s|engines apps test|engines|" Makefile.org || exit 1
 endef
 
 define $(package)_config_cmds
-  ./Configure $($(package)_config_opts)
+  ./Configure $($(package)_config_opts) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc
+  $(MAKE) -j1 build_libs libcrypto.pc libssl.pc openssl.pc || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw
+  $(MAKE) INSTALL_PREFIX=$($(package)_staging_dir) -j1 install_sw || exit 1
 endef
 
 define $(package)_postprocess_cmds
-  rm -rf share bin etc
+  rm -rf $($(package)_staging_dir)$(host_prefix)/{share,bin,etc} || exit 1
 endef
