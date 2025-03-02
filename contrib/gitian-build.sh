5aab449c6b5e0f6cceac6cab1eb18965cf27885c
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 21:32:10 -0500

    Update gitian-build.sh
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -1,4 +1,5 @@
-# Copyright (c) 2016 The Bitcoin Core developers
+#!/bin/bash
+# Copyright (c) 2016-2025 The LPSCoin Developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -11,382 +12,244 @@ setupenv=false
 # Systems to build
 linux=true
 windows=true
-osx=true
+macos=true
 
-# Other Basic variables
+# Basic variables
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/lpscoins-project/lpscoins
+url=https://github.com/Luxury-Property-Solutions-LLC/LPSCoin
 proc=2
 mem=2000
 lxc=true
-osslTarUrl=http://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-1.7.1.tar.gz
-osslPatchUrl=https://bitcoincore.org/cfields/osslsigncode-Backports-to-1.7.1.patch
+osslTarUrl=https://downloads.sourceforge.net/project/osslsigncode/osslsigncode/osslsigncode-2.1.tar.gz
+osslPatchUrl=https://bitcoincore.org/cfields/osslsigncode-Backports-to-2.1.patch
 scriptName=$(basename -- "$0")
 signProg="gpg --detach-sign"
 commitFiles=true
 
 # Help Message
 read -d '' usage <<- EOF
-Usage: $scriptName [-c|u|v|b|s|B|o|h|j|m|] signer version
+Usage: $scriptName [-c|u|v|b|s|B|o|h|j|m] signer version
 
-Run this script from the directory containing the lpscoins, gitian-builder, gitian.sigs, and lpscoins-detached-sigs.
+Run this script from the directory containing the lpscoin, gitian-builder, gitian.sigs, and lpscoin-detached-sigs repositories.
 
 Arguments:
 signer          GPG signer to sign each build assert file
-version		Version number, commit, or branch to build. If building a commit or branch, the -c option must be specified
+version         Version number, commit, or branch to build. Use -c for commits or branches
 
 Options:
--c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/lpscoins-project/lpscoins
--v|--verify 	Verify the gitian build
--b|--build	Do a gitian build
--s|--sign	Make signed binaries for Windows and Mac OSX
--B|--buildsign	Build both signed and unsigned binaries
--o|--os		Specify which Operating Systems the build is for. Default is lwx. l for linux, w for windows, x for osx
--j		Number of processes to use. Default 2
--m		Memory to allocate in MiB. Default 2000
+-c|--commit     Indicate that the version argument is a commit or branch
+-u|--url        Specify the repository URL (default: $url)
+-v|--verify     Verify the Gitian build
+-b|--build      Perform a Gitian build
+-s|--sign       Make signed binaries for Windows and macOS
+-B|--buildsign  Build both signed and unsigned binaries
+-o|--os         Specify OSes to build: 'l' for Linux, 'w' for Windows, 'x' for macOS (default: lwx)
+-j              Number of processes to use (default: $proc)
+-m              Memory to allocate in MiB (default: $mem)
 --kvm           Use KVM instead of LXC
---setup         Setup the gitian building environment. Uses KVM. If you want to use lxc, use the --lxc option. Only works on Debian-based systems (Ubuntu, Debian)
---detach-sign   Create the assert file for detached signing. Will not commit anything.
---no-commit     Do not commit anything to git
--h|--help	Print this help message
+--setup         Set up the Gitian environment (uses KVM unless --lxc is specified; Debian/Ubuntu only)
+--detach-sign   Create assert files for detached signing without committing
+--no-commit     Do not commit to git
+-h|--help       Print this help message
 EOF
 
-# Get options and arguments
+# Parse options and arguments
 while :; do
     case $1 in
-        # Verify
-        -v|--verify)
-	    verify=true
-            ;;
-        # Build
-        -b|--build)
-	    build=true
-            ;;
-        # Sign binaries
-        -s|--sign)
-	    sign=true
-            ;;
-        # Build then Sign
-        -B|--buildsign)
-	    sign=true
-	    build=true
-            ;;
-        # PGP Signer
+        -v|--verify) verify=true ;;
+        -b|--build) build=true ;;
+        -s|--sign) sign=true ;;
+        -B|--buildsign) sign=true; build=true ;;
         -S|--signer)
-	    if [ -n "$2" ]
-	    then
-		SIGNER=$2
-		shift
-	    else
-		echo 'Error: "--signer" requires a non-empty argument.'
-		exit 1
-	    fi
-           ;;
-        # Operating Systems
+            if [ -n "$2" ]; then SIGNER=$2; shift; else echo "Error: '--signer' requires a non-empty argument."; exit 1; fi ;;
         -o|--os)
-	    if [ -n "$2" ]
-	    then
-		linux=false
-		windows=false
-		osx=false
-		if [[ "$2" = *"l"* ]]
-		then
-		    linux=true
-		fi
-		if [[ "$2" = *"w"* ]]
-		then
-		    windows=true
-		fi
-		if [[ "$2" = *"x"* ]]
-		then
-		    osx=true
-		fi
-		shift
-	    else
-		echo 'Error: "--os" requires an argument containing an l (for linux), w (for windows), or x (for Mac OSX)\n'
-		exit 1
-	    fi
-	    ;;
-	# Help message
-	-h|--help)
-	    echo "$usage"
-	    exit 0
-	    ;;
-	# Commit or branch
-	-c|--commit)
-	    commit=true
-	    ;;
-	# Number of Processes
-	-j)
-	    if [ -n "$2" ]
-	    then
-		proc=$2
-		shift
-	    else
-		echo 'Error: "-j" requires an argument'
-		exit 1
-	    fi
-	    ;;
-	# Memory to allocate
-	-m)
-	    if [ -n "$2" ]
-	    then
-		mem=$2
-		shift
-	    else
-		echo 'Error: "-m" requires an argument'
-		exit 1
-	    fi
-	    ;;
-	# URL
-	-u)
-	    if [ -n "$2" ]
-	    then
-		url=$2
-		shift
-	    else
-		echo 'Error: "-u" requires an argument'
-		exit 1
-	    fi
-	    ;;
-        # kvm
-        --kvm)
-            lxc=false
-            ;;
-        # Detach sign
-        --detach-sign)
-            signProg="true"
-            commitFiles=false
-            ;;
-        # Commit files
-        --no-commit)
-            commitFiles=false
-            ;;
-        # Setup
-        --setup)
-            setup=true
-            ;;
-	*)               # Default case: If no more options then break out of the loop.
-             break
+            if [ -n "$2" ]; then
+                linux=false; windows=false; macos=false
+                [[ "$2" =~ "l" ]] && linux=true
+                [[ "$2" =~ "w" ]] && windows=true
+                [[ "$2" =~ "x" ]] && macos=true
+                shift
+            else echo "Error: '--os' requires an argument with 'l' (Linux), 'w' (Windows), or 'x' (macOS)"; exit 1; fi ;;
+        -h|--help) echo "$usage"; exit 0 ;;
+        -c|--commit) commit=true ;;
+        -j) if [ -n "$2" ]; then proc=$2; shift; else echo "Error: '-j' requires an argument"; exit 1; fi ;;
+        -m) if [ -n "$2" ]; then mem=$2; shift; else echo "Error: '-m' requires an argument"; exit 1; fi ;;
+        -u) if [ -n "$2" ]; then url=$2; shift; else echo "Error: '-u' requires an argument"; exit 1; fi ;;
+        --kvm) lxc=false ;;
+        --detach-sign) signProg="true"; commitFiles=false ;;
+        --no-commit) commitFiles=false ;;
+        --setup) setupenv=true ;;
+        *) break ;;
     esac
     shift
 done
 
 # Set up LXC
-if [[ $lxc = true ]]
-then
+if [[ $lxc = true ]]; then
     export USE_LXC=1
     export LXC_BRIDGE=lxcbr0
     sudo ifconfig lxcbr0 up 10.0.2.2
 fi
 
-# Check for OSX SDK
-if [[ ! -e "gitian-builder/inputs/MacOSX10.11.sdk.tar.gz" && $osx == true ]]
-then
-    echo "Cannot build for OSX, SDK does not exist. Will build for other OSes"
-    osx=false
+# Check for macOS SDK
+if [[ ! -e "gitian-builder/inputs/MacOSX10.11.sdk.tar.gz" && $macos = true ]]; then
+    echo "Cannot build for macOS, SDK does not exist. Will build for other OSes"
+    macos=false
 fi
 
-# Get signer
-if [[ -n"$1" ]]
-then
-    SIGNER=$1
-    shift
-fi
+# Get signer and version
+SIGNER=${1:-}
+shift
+VERSION=${1:-}
+COMMIT=$VERSION
 
-# Get version
-if [[ -n "$1" ]]
-then
-    VERSION=$1
-    COMMIT=$VERSION
-    shift
-fi
-
-# Check that a signer is specified
-if [[ $SIGNER == "" ]]
-then
+# Validate inputs
+if [[ -z "$SIGNER" ]]; then
     echo "$scriptName: Missing signer."
     echo "Try $scriptName --help for more information"
     exit 1
 fi
-
-# Check that a version is specified
-if [[ $VERSION == "" ]]
-then
+if [[ -z "$VERSION" ]]; then
     echo "$scriptName: Missing version."
     echo "Try $scriptName --help for more information"
     exit 1
 fi
 
-# Add a "v" if no -c
-if [[ $commit = false ]]
-then
-	COMMIT="v${VERSION}"
-fi
-echo ${COMMIT}
+# Add "v" prefix if not a commit
+[[ $commit = false ]] && COMMIT="v${VERSION}"
+echo "Building commit: $COMMIT"
 
 # Setup build environment
-if [[ $setup = true ]]
-then
-    sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/lpscoins-project/gitian.sigs.git
-    git clone https://github.com/lpscoins-project/lpscoins-detached-sigs.git
+if [[ $setupenv = true ]]; then
+    sudo apt-get update
+    sudo apt-get install -y ruby apache2 git apt-cacher-ng python3-pyvmomi qemu-kvm qemu-utils
+    git clone https://github.com/Luxury-Property-Solutions-LLC/gitian.sigs.git
+    git clone https://github.com/Luxury-Property-Solutions-LLC/lpscoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
-    if [[ -n "$USE_LXC" ]]
-    then
-        sudo apt-get install lxc
-        bin/make-base-vm --suite trusty --arch amd64 --lxc
+    if [[ -n "$USE_LXC" ]]; then
+        sudo apt-get install -y lxc
+        bin/make-base-vm --suite focal --arch amd64 --lxc
     else
-        bin/make-base-vm --suite trusty --arch amd64
+        bin/make-base-vm --suite focal --arch amd64
     fi
     popd
 fi
 
 # Set up build
-pushd ./lpscoins
+pushd ./lpscoin
 git fetch
 git checkout ${COMMIT}
 popd
 
 # Build
-if [[ $build = true ]]
-then
-	# Make output folder
-	mkdir -p ./lpscoins-binaries/${VERSION}
+if [[ $build = true ]]; then
+    mkdir -p ./lpscoin-binaries/${VERSION}
+    echo ""
+    echo "Building Dependencies"
+    echo ""
+    pushd ./gitian-builder
+    mkdir -p inputs
+    wget -N -P inputs $osslPatchUrl
+    wget -N -P inputs $osslTarUrl
+    make -C ../lpscoin/depends download SOURCES_PATH=$(pwd)/cache/common
 
-	# Build Dependencies
-	echo ""
-	echo "Building Dependencies"
-	echo ""
-	pushd ./gitian-builder
-	mkdir -p inputs
-	wget -N -P inputs $osslPatchUrl
-	wget -N -P inputs $osslTarUrl
-	make -C ../lpscoins/depends download SOURCES_PATH=`pwd`/cache/common
+    # Linux
+    if [[ $linux = true ]]; then
+        echo ""
+        echo "Compiling ${VERSION} Linux"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoin=${COMMIT} --url lpscoin=${url} ../lpscoin/contrib/gitian-descriptors/gitian-linux.yml
+        ./bin/gsign -p "$signProg" --signer "$SIGNER" --release ${VERSION}-linux --destination ../gitian.sigs/ ../lpscoin/contrib/gitian-descriptors/gitian-linux.yml
+        mv build/out/lpscoin-*.tar.gz build/out/src/lpscoin-*.tar.gz ../lpscoin-binaries/${VERSION}
+    fi
 
-	# Linux
-	if [[ $linux = true ]]
-	then
-            echo ""
-	    echo "Compiling ${VERSION} Linux"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoins=${COMMIT} --url lpscoins=${url} ../lpscoins/contrib/gitian-descriptors/gitian-linux.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../lpscoins/contrib/gitian-descriptors/gitian-linux.yml
-	    mv build/out/lpscoins-*.tar.gz build/out/src/lpscoins-*.tar.gz ../lpscoins-binaries/${VERSION}
-	fi
-	# Windows
-	if [[ $windows = true ]]
-	then
-	    echo ""
-	    echo "Compiling ${VERSION} Windows"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoins=${COMMIT} --url lpscoins=${url} ../lpscoins/contrib/gitian-descriptors/gitian-win.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../lpscoins/contrib/gitian-descriptors/gitian-win.yml
-	    mv build/out/lpscoins-*-win-unsigned.tar.gz inputs/lpscoins-win-unsigned.tar.gz
-	    mv build/out/lpscoins-*.zip build/out/lpscoins-*.exe ../lpscoins-binaries/${VERSION}
-	fi
-	# Mac OSX
-	if [[ $osx = true ]]
-	then
-	    echo ""
-	    echo "Compiling ${VERSION} Mac OSX"
-	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoins=${COMMIT} --url lpscoins=${url} ../lpscoins/contrib/gitian-descriptors/gitian-osx.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../lpscoins/contrib/gitian-descriptors/gitian-osx.yml
-	    mv build/out/lpscoins-*-osx-unsigned.tar.gz inputs/lpscoins-osx-unsigned.tar.gz
-	    mv build/out/lpscoins-*.tar.gz build/out/lpscoins-*.dmg ../lpscoins-binaries/${VERSION}
-	fi
-	popd
+    # Windows
+    if [[ $windows = true ]]; then
+        echo ""
+        echo "Compiling ${VERSION} Windows"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoin=${COMMIT} --url lpscoin=${url} ../lpscoin/contrib/gitian-descriptors/gitian-win.yml
+        ./bin/gsign -p "$signProg" --signer "$SIGNER" --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../lpscoin/contrib/gitian-descriptors/gitian-win.yml
+        mv build/out/lpscoin-*-win-unsigned.tar.gz inputs/lpscoin-win-unsigned.tar.gz
+        mv build/out/lpscoin-*.zip build/out/lpscoin-*.exe ../lpscoin-binaries/${VERSION}
+    fi
 
-        if [[ $commitFiles = true ]]
-        then
-	    # Commit to gitian.sigs repo
-            echo ""
-            echo "Committing ${VERSION} Unsigned Sigs"
-            echo ""
-            pushd gitian.sigs
-            git add ${VERSION}-linux/${SIGNER}
-            git add ${VERSION}-win-unsigned/${SIGNER}
-            git add ${VERSION}-osx-unsigned/${SIGNER}
-            git commit -a -m "Add ${VERSION} unsigned sigs for ${SIGNER}"
-            popd
-        fi
+    # macOS
+    if [[ $macos = true ]]; then
+        echo ""
+        echo "Compiling ${VERSION} macOS"
+        echo ""
+        ./bin/gbuild -j ${proc} -m ${mem} --commit lpscoin=${COMMIT} --url lpscoin=${url} ../lpscoin/contrib/gitian-descriptors/gitian-osx.yml
+        ./bin/gsign -p "$signProg" --signer "$SIGNER" --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../lpscoin/contrib/gitian-descriptors/gitian-osx.yml
+        mv build/out/lpscoin-*-osx-unsigned.tar.gz inputs/lpscoin-osx-unsigned.tar.gz
+        mv build/out/lpscoin-*.tar.gz build/out/lpscoin-*.dmg ../lpscoin-binaries/${VERSION}
+    fi
+    popd
+
+    if [[ $commitFiles = true ]]; then
+        echo ""
+        echo "Committing ${VERSION} Unsigned Sigs"
+        echo ""
+        pushd gitian.sigs
+        git add ${VERSION}-linux/${SIGNER}
+        git add ${VERSION}-win-unsigned/${SIGNER}
+        git add ${VERSION}-osx-unsigned/${SIGNER}
+        git commit -a -m "Add ${VERSION} unsigned sigs for ${SIGNER}"
+        popd
+    fi
 fi
 
 # Verify the build
-if [[ $verify = true ]]
-then
-	# Linux
-	pushd ./gitian-builder
-	echo ""
-	echo "Verifying v${VERSION} Linux"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../lpscoins/contrib/gitian-descriptors/gitian-linux.yml
-	# Windows
-	echo ""
-	echo "Verifying v${VERSION} Windows"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../lpscoins/contrib/gitian-descriptors/gitian-win.yml
-	# Mac OSX
-	echo ""
-	echo "Verifying v${VERSION} Mac OSX"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../lpscoins/contrib/gitian-descriptors/gitian-osx.yml
-	# Signed Windows
-	echo ""
-	echo "Verifying v${VERSION} Signed Windows"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../lpscoins/contrib/gitian-descriptors/gitian-osx-signer.yml
-	# Signed Mac OSX
-	echo ""
-	echo "Verifying v${VERSION} Signed Mac OSX"
-	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../lpscoins/contrib/gitian-descriptors/gitian-osx-signer.yml
-	popd
+if [[ $verify = true ]]; then
+    pushd ./gitian-builder
+    echo ""
+    echo "Verifying v${VERSION} Linux"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../lpscoin/contrib/gitian-descriptors/gitian-linux.yml
+    echo ""
+    echo "Verifying v${VERSION} Windows"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../lpscoin/contrib/gitian-descriptors/gitian-win.yml
+    echo ""
+    echo "Verifying v${VERSION} macOS"
+    echo ""
+    ./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../lpscoin/contrib/gitian-descriptors/gitian-osx.yml
+    popd
 fi
 
 # Sign binaries
-if [[ $sign = true ]]
-then
+if [[ $sign = true ]]; then
+    pushd ./gitian-builder
+    if [[ $windows = true ]]; then
+        echo ""
+        echo "Signing ${VERSION} Windows"
+        echo ""
+        ./bin/gbuild -i --commit signature=${COMMIT} ../lpscoin/contrib/gitian-descriptors/gitian-win-signer.yml
+        ./bin/gsign -p "$signProg" --signer "$SIGNER" --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../lpscoin/contrib/gitian-descriptors/gitian-win-signer.yml
+        mv build/out/lpscoin-*win64-setup.exe ../lpscoin-binaries/${VERSION}
+        mv build/out/lpscoin-*win32-setup.exe ../lpscoin-binaries/${VERSION}
+    fi
+    if [[ $macos = true ]]; then
+        echo ""
+        echo "Signing ${VERSION} macOS"
+        echo ""
+        ./bin/gbuild -i --commit signature=${COMMIT} ../lpscoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+        ./bin/gsign -p "$signProg" --signer "$SIGNER" --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../lpscoin/contrib/gitian-descriptors/gitian-osx-signer.yml
+        mv build/out/lpscoin-osx-signed.dmg ../lpscoin-binaries/${VERSION}/lpscoin-${VERSION}-osx.dmg
+    fi
+    popd
 
-        pushd ./gitian-builder
-	# Sign Windows
-	if [[ $windows = true ]]
-	then
-	    echo ""
-	    echo "Signing ${VERSION} Windows"
-	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../lpscoins/contrib/gitian-descriptors/gitian-win-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../lpscoins/contrib/gitian-descriptors/gitian-win-signer.yml
-	    mv build/out/lpscoins-*win64-setup.exe ../lpscoins-binaries/${VERSION}
-	    mv build/out/lpscoins-*win32-setup.exe ../lpscoins-binaries/${VERSION}
-	fi
-	# Sign Mac OSX
-	if [[ $osx = true ]]
-	then
-	    echo ""
-	    echo "Signing ${VERSION} Mac OSX"
-	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../lpscoins/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../lpscoins/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    mv build/out/lpscoins-osx-signed.dmg ../lpscoins-binaries/${VERSION}/lpscoins-${VERSION}-osx.dmg
-	fi
-	popd
-
-        if [[ $commitFiles = true ]]
-        then
-            # Commit Sigs
-            pushd gitian.sigs
-            echo ""
-            echo "Committing ${VERSION} Signed Sigs"
-            echo ""
-            git add ${VERSION}-win-signed/${SIGNER}
-            git add ${VERSION}-osx-signed/${SIGNER}
-            git commit -a -m "Add ${VERSION} signed binary sigs for ${SIGNER}"
-            popd
-        fi
+    if [[ $commitFiles = true ]]; then
+        echo ""
+        echo "Committing ${VERSION} Signed Sigs"
+        echo ""
+        pushd gitian.sigs
+        git add ${VERSION}-win-signed/${SIGNER}
+        git add ${VERSION}-osx-signed/${SIGNER}
+        git commit -a -m "Add ${VERSION} signed binary sigs for ${SIGNER}"
+        popd
+    fi
 fi
