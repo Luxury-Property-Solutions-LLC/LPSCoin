95aa8575fea45a1fec64d55ea95d08b46f67eb06
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:59:59 -0500

    Update zlib.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -1,26 +1,30 @@
 package=zlib
-$(package)_version=1.2.11
-$(package)_download_path=https://www.zlib.net
+$(package)_version=1.3.1
+$(package)_download_path=https://github.com/madler/zlib/releases/download/v$($(package)_version)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+$(package)_sha256_hash=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df63
 
 define $(package)_set_vars
-$(package)_build_opts= CC="$($(package)_cc)"
-$(package)_build_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -fPIC"
-$(package)_build_opts+=RANLIB="$($(package)_ranlib)"
-$(package)_build_opts+=AR="$($(package)_ar)"
-$(package)_build_opts_darwin+=AR="$($(package)_libtool)"
-$(package)_build_opts_darwin+=ARFLAGS="-o"
+  $(package)_build_opts=CC="$($(package)_cc)"
+  $(package)_build_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -fPIC"
+  $(package)_build_opts+=RANLIB="$($(package)_ranlib)"
+  $(package)_build_opts+=AR="$($(package)_ar)"
+  $(package)_build_opts_darwin+=AR="$($(package)_libtool)"
+  $(package)_build_opts_darwin+=ARFLAGS="-o"
 endef
 
 define $(package)_config_cmds
-  ./configure --static --prefix=$(host_prefix)
+  ./configure --static --prefix=$(host_prefix) || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE) $($(package)_build_opts) libz.a
+  $(MAKE) $($(package)_build_opts) libz.a || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install $($(package)_build_opts)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install $($(package)_build_opts) || exit 1
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf $($(package)_staging_dir)$(host_prefix)/share || exit 1
 endef
