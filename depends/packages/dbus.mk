67f9856a309492bfaac9abbf13078ec7f4df7f52
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 22:15:58 -0500

    Update dbus.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/dbus.mk
+++ depends/packages/dbus.mk
@@ -1,12 +1,14 @@
 package=dbus
-$(package)_version=1.8.6
-$(package)_download_path=http://dbus.freedesktop.org/releases/dbus
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=eded83ca007b719f32761e60fd8b9ffd0f5796a4caf455b01b5a5ef740ebd23f
-$(package)_dependencies=expat
+$(package)_version:=1.14.10
+$(package)_download_path:=https://dbus.freedesktop.org/releases/dbus
+$(package)_file_name:=$(package)-$($(package)_version).tar.xz
+$(package)_sha256_hash:=ba1b02f5586cef279686964bb0fe35a270e91e9d928d87d21a6a7d95625caaec
+$(package)_dependencies:=expat
+$(package)_autoconf:=configure
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-tests --disable-doxygen-docs --disable-xml-docs --disable-static --without-x
+  $(package)_config_opts:=--disable-tests --disable-doxygen-docs --disable-xml-docs --disable-shared --without-x
+  $(package)_config_opts+=--with-expat=$($(package)_staging_prefix_dir)
 endef
 
 define $(package)_config_cmds
@@ -14,10 +16,10 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C dbus libdbus-1.la
+  $(MAKE) -C dbus libdbus-1.la || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) -C dbus DESTDIR=$($(package)_staging_dir) install-libLTLIBRARIES install-dbusincludeHEADERS install-nodist_dbusarchincludeHEADERS && \
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install-pkgconfigDATA
+  $(MAKE) -C dbus DESTDIR=$($(package)_staging_dir) install-libLTLIBRARIES install-dbusincludeHEADERS install-nodist_dbusarchincludeHEADERS || exit 1 && \
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install-pkgconfigDATA || exit 1
 endef
