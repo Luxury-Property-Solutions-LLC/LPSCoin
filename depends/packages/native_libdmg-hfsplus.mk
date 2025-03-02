ee7b896ce2b388a6424d9b2f028522eece8d1bae
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:28:06 -0500

    Update native_libdmg-hfsplus.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/native_libdmg-hfsplus.mk
+++ depends/packages/native_libdmg-hfsplus.mk
@@ -6,17 +6,16 @@ $(package)_sha256_hash=6569a02eb31c2827080d7d59001869ea14484c281efab0ae7f2b86af5
 $(package)_build_subdir=build
 
 define $(package)_preprocess_cmds
-  mkdir build
 endef
 
 define $(package)_config_cmds
-  cmake -DCMAKE_INSTALL_PREFIX:PATH=$(build_prefix)/bin ..
+  cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix) .. || exit 1
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -C dmg
+  $(MAKE) -C dmg || exit 1
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) -C dmg install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) -C dmg install || exit 1
 endef
