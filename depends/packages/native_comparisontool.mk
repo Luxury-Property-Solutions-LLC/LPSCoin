a2a897e325c5b1fac781f1931640cab89173742a
LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>
2025-03-01 23:23:04 -0500

    Update native_comparisontool.mk
    Signed-off-by: LPSLAMAADMIN <112085531+LPSLAMAADMIN@users.noreply.github.com>

--- depends/packages/native_comparisontool.mk
+++ depends/packages/native_comparisontool.mk
@@ -1,6 +1,6 @@
 package=native_comparisontool
 $(package)_version=8c6666f
-$(package)_download_path=https://github.com/theuni/bitcoind-comparisontool/raw/master
+$(package)_download_path=https://github.com/theuni/bitcoind-comparisontool/raw/8c6666f8af91e5be5b5d338a3d2e97f54d6cc875
 $(package)_file_name=pull-tests-$($(package)_version).jar
 $(package)_sha256_hash=a865332b3827abcde684ab79f5f43c083b0b6a4c97ff5508c79f29fee24f11cd
 $(package)_install_dirname=BitcoindComparisonTool_jar
@@ -16,6 +16,6 @@ define $(package)_build_cmds
 endef
 
 define $(package)_stage_cmds
-  mkdir -p $($(package)_staging_prefix_dir)/share/$($(package)_install_dirname) && \
-  cp $($(package)_source) $($(package)_staging_prefix_dir)/share/$($(package)_install_dirname)/$($(package)_install_filename)
+  mkdir -p $($(package)_staging_prefix_dir)/share/$($(package)_install_dirname) || exit 1 && \
+  cp $($(package)_source) $($(package)_staging_prefix_dir)/share/$($(package)_install_dirname)/$($(package)_install_filename) || exit 1
 endef
